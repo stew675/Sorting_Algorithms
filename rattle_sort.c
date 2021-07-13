a95fcf51302ecebbab38899f779ee8994b7c4846
Stew Forster <sforster@fastly.com>
2021-07-13 10:19:00 -0500

    Rattle Sort tweaks

--- rattle_sort.c
+++ rattle_sort.c
@@ -7,24 +7,29 @@ rattle_sort(register char *a, size_t n, register const size_t es, register const
 	register char	*b, *c, *e = a + (n - 1) * es;
 	register int	swaptype;
 
-	if (n < 2) return;
+	if (n < 2)
+		return;
+
+	n = (n/3)+1;
 
 	SWAPINIT(a, es);
 
-	for (;;) {
+	// Attempt to move elements fairly close to where they should finally be
+	for(;;) {
 		// Forward sift
-		if ((n = (n * 13) / 17) < 2) break;
-		for (b = a, c = a + (n * es); c <= e; b+=es, c+=es)
+		for (b=a, c=a+(n*es); c<=e; b+=es, c+=es)
 			if (cmp(b, c) > 0)
 				swap(b, c);
+		if ((n=(n*13)/17) < 2) break;
 
 		// Backward sift
-		if ((n = (n * 13) / 17) < 2) break;
-		for (b = e, c = e - (n * es); c >= a; b-=es, c-=es)
+		for (b=e, c=e-(n*es); c>=a; b-=es, c-=es)
 			if (cmp(b, c) < 0)
 				swap(b, c);
+		if ((n=(n*13)/17) < 2) break;
 	}
 
+	// 2-way bubble sort with closing ends to mop up any stragglers
 	for (char *saved;;) {
 		// Forward sift
 		for (saved = a, b = a, c = a + es; c <= e; b=c, c+=es)
