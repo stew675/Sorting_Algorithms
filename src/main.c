d9faaf7f9eca172e858f6ea27d39d79c9fe06a91
Stew Forster <sforster@fastly.com>
2021-07-25 07:39:30 -0500

    Improved data set generation algorithm

--- src/main.c
+++ src/main.c
@@ -59,10 +59,10 @@ compare_uint32(register const void *p1, register const void *p2)
 static void
 print_array(register uint32_t a[], register size_t n)
 {
-	printf("\n%u", a[0]);
+	printf("\nDATA_SET = [%u", a[0]);
 	for(register size_t i = 1; i < n; i++)
 		printf(", %u", a[i]);
-	printf("\n\n");
+	printf("]\n");
 } // print_array
 
 
@@ -87,12 +87,12 @@ usage(char *prog, char *msg)
 	fprintf(stderr, "\t            A value of 0 will use a randomly generated seed\n");
 	fprintf(stderr, "\t-d <0..100> Disorder the generated set by the percentage given (default=100)\n");
 	fprintf(stderr, "\t-f          Data set keys/values range from 0..UINT32_MAX (default)\n");
-	fprintf(stderr, "\t-l <1..MAX> Data set keys/values limited in range from 0..num\n");
+	fprintf(stderr, "\t-l <num>    Data set keys/values limited in range from 0..(num-1)\n");
 	fprintf(stderr, "\t-l n        If the letter 'n' is specified, use the number of elements as the key range\n");
-	fprintf(stderr, "\t-o          Use a fully ordered data set (sets disorder factor to 0)\n");
+	fprintf(stderr, "\t-o          Use a fully ordered data set (Shorthand for setting disorder factor to 0)\n");
 	fprintf(stderr, "\t-r          Reverse the data set order after generating it\n");
 	fprintf(stderr, "\t-u          Data set keys/values must all be unique\n");
-	fprintf(stderr, "\t-v          Verbose.  Display the data set after generating it\n");
+	fprintf(stderr, "\t-v          Verbose.  Display the data set before sorting it\n");
 	fprintf(stderr, "\n<sort_type> is one only of the following options\n");
 	fprintf(stderr, "\t-bb\tBidirectional Bubble Sort\n");
 	fprintf(stderr, "\t-bs\tBidirectional Bubble Shell Sort\n");
@@ -270,18 +270,26 @@ reverse_set(uint32_t *a, size_t n)
 
 // Perform a disorder on the set
 void
-disorder_pass(uint32_t *a, size_t n)
+disorder_set(uint32_t *a, size_t n)
 {
+	if (disorder_factor <= 0)
+		return;
+	if (disorder_factor > 100)
+		disorder_factor = 100;
+
+	// Fowards pass
 	for (size_t i = 0; i < (n - 1); i++) {
 		size_t target, range;
 		uint32_t t;
 
 		// Determine if we will disorder this element
+		// We halve the disorder factor because we do
+		// a second reverse pass just below
 		if ((random() % 100) >= ((disorder_factor + 1) / 2))
 			continue;
 
-		// Establish the range will be for the disorder
-		range = n - i;
+		// Establish the range for the disorder
+		range = (n - i) - 1;
 		range *= disorder_factor;
 		range /= 100;
 		range += 1;
@@ -294,21 +302,30 @@ disorder_pass(uint32_t *a, size_t n)
 		a[i] = a[target];
 		a[target] = t;
 	}
-} // disorder_pass
 
+	// Backwards pass
+	for (size_t i = n - 1; i > 0; i--) {
+		size_t target, range;
+		uint32_t t;
 
-void
-disorder_set(uint32_t *a, size_t n)
-{
-	if (disorder_factor <= 0)
-		return;
-	if (disorder_factor > 100)
-		disorder_factor = 100;
+		// Determine if we will disorder this element
+		if ((random() % 100) >= ((disorder_factor + 1) / 2))
+			continue;
 
-	disorder_pass(a, n);
-	reverse_set(a, n);
-	disorder_pass(a, n);
-	reverse_set(a, n);
+		// Establish the range for the disorder
+		range = i - 1;
+		range *= disorder_factor;
+		range /= 100;
+		range += 1;
+		do {
+			target = (i - (random() % range)) - 1;
+		} while (target >= n);
+
+		// We have our target. Swap ourselves with that
+		t = a[i];
+		a[i] = a[target];
+		a[target] = t;
+	}
 } // disorder_set
 
 
@@ -317,9 +334,7 @@ get_next_val(uint32_t val, size_t pos, size_t n)
 {
 	uint32_t vals_left = data_set_limit - val;
 	size_t togo = n - pos;
-	double step = vals_left;
-
-	step /= togo;
+	double step;
 
 	// Bump step by 1 if it's >1, and if we're not generating
 	// unique numbers. This reduces the chance we'll generate
@@ -328,25 +343,48 @@ get_next_val(uint32_t val, size_t pos, size_t n)
 	// not increment val, whereas if we do this, then we will
 	// at least ~53% of the time
 	if (data_set_ops & DATA_SET_UNIQUE) {
-		if (step < 1) {
+		if (vals_left < togo) {
 			fprintf(stderr, "Data set requested is larger than the unique\n");
 			fprintf(stderr, "set of values that can be generated. Aborting\n");
 			exit(1);
 		}
-	} else if (vals_left > 1) {
+		step = vals_left - 1;
+		step /= togo;
 		step += 1;
+		if (step > vals_left) {
+			step = vals_left;
+		}
+	} else {
+		vals_left -= 1;
+		if (vals_left > 0) {
+			step = data_set_limit;
+			step /= n;
+			if (vals_left > togo) {
+				step += 2;
+			} else {
+				step += 1;
+			}
+			if (step > (vals_left + 1)) {
+				step = vals_left + 1;
+			}
+		} else {
+			step = 0;
+		}
 	}
 
-	// Generate a decimal value in the range 0..1
-	double r = (double)random();
-	r /= RAND_MAX;
+	// Generate a decimal value in the range 0..1 (but excluding 1 itself)
+	double r = (double)random(), rm = RAND_MAX;
+	rm += 1;
+	r /= rm;
 
 	step *= r;
 
 	if (pos == 0) {
 		if (data_set_ops & DATA_SET_UNIQUE) {
-			if (step < 1) {
-				return 1;
+			if (step > 2) {
+				return (step - 1);
+			} else {
+				return 0;
 			}
 		}
 		return (uint32_t)step;
@@ -355,8 +393,6 @@ get_next_val(uint32_t val, size_t pos, size_t n)
 	if (data_set_ops & DATA_SET_UNIQUE) {
 		if (step < 1)
 			step = 1;
-		if (step > 1)
-			step += 1;
 	}
 
 	return val + (uint32_t)step;
@@ -418,16 +454,18 @@ main(int argc, char *argv[])
 		data_set_limit = n;
 	}
 
+	// Just print a clear line before everything else
+	printf("\n");
+
 	// Set up the random number generator
-	if (random_seed > 0) {
-		printf("Seeding Random Generator with:  %u\n", random_seed);
-	} else {
+	if (random_seed <= 0) {
 		struct timespec now;
 
 		clock_gettime(CLOCK_MONOTONIC, &now);
 		random_seed = (uint32_t)((now.tv_sec ^ now.tv_nsec) & 0xffffffff);
-		printf("Using randomly generated seed value: %u\n", random_seed);
+		printf("Using a randomly generated random seed value\n");
 	}
+	printf("Seeding Random Generator with:  %u\n", random_seed);
 	srandom(random_seed);
 
 	// Allocate up the array to sort
@@ -439,6 +477,20 @@ main(int argc, char *argv[])
 
 	// Now populate the array according to the command line options
 	printf("\nPopulating array of size: %lu\n\n", n);
+	printf("Data value range is 0..%u (inclusive)\n", data_set_limit - 1);
+	if (disorder_factor == 0) {
+		printf("Data set is ordered\n");
+	} else {
+		printf("Data set is disordered with factor: %d%%\n", disorder_factor);
+	}
+	if (data_set_ops & DATA_SET_UNIQUE) {
+		printf("All data values are unique\n");
+	} else {
+		printf("Duplicate data values are allowed in the set\n");
+	}
+	if (data_set_ops & DATA_SET_REVERSED) {
+		printf("Data set is reversed\n");
+	}
 
 	fillset(a, n);
 	if (verbose) {
@@ -447,7 +499,7 @@ main(int argc, char *argv[])
 
 	// Let's finally do this thing!
 	struct timespec start, end;
-	printf("Starting %s\n", sortname);
+	printf("\nStarting %s\n", sortname);
 	clock_gettime(CLOCK_MONOTONIC, &start);
 	if (sort != qrsort) {
 		if (sort == GrailSort) {
